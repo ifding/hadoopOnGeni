dd6017724920e7efbc4b3d94c0d9c58acb4085e0
ifding <fding87@gmail.com>
2017-11-06 12:31:07 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ YARN_RESOURCEMANAGER="$HDFS_NAMENODE"
 ZOOKEEPER_QUORUM="$HDFS_NAMENODE"
 
 #get the hostname of DataNode
-if ! [ "${hostname#datanode}" = "${hostname}" ]; then
+if ! [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
   echo "get datanode hostname";
   HDFS_DATANODE="$HDFS_HOSTNAME"
   YARN_NODEMANAGER="$HDFS_DATANODE"
@@ -60,20 +60,19 @@ source scripts/users.sh
 
 #create hadoop group and system users
 echo "Creating Hadoop group . . ."
-groupadd $HADOOP_GROUP
+sudo groupadd $HADOOP_GROUP
 echo "Creating Hadoop system accounts . . ."
-useradd -g $HADOOP_GROUP $HDFS_USER
-useradd -g $HADOOP_GROUP $MAPRED_USER
-useradd -g $HADOOP_GROUP $TEMPLETON_USER
-useradd -g $HADOOP_GROUP $ZOOKEEPER_USER
-sleep 3
-
+sudo useradd -g $HADOOP_GROUP $HDFS_USER
+sudo useradd -g $HADOOP_GROUP $MAPRED_USER
+sudo useradd -g $HADOOP_GROUP $TEMPLETON_USER
+sudo useradd -g $HADOOP_GROUP $ZOOKEEPER_USER
+#sleep 3
 
 #install hadoop core packages from yum
 sudo yum install -y hadoop hadoop-hdfs hadoop-libhdfs hadoop-yarn hadoop-mapreduce hadoop-client openssl
 
 #NameNode directories
-if ! [ "${hostname#namenode}" = "${hostname}" ]; then
+if [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
   echo "creating namenode directories";
   sudo mkdir -p $DFS_NAME_DIR;
   sudo chown -R $HDFS_USER:$HADOOP_GROUP $DFS_NAME_DIR;
@@ -81,7 +80,7 @@ if ! [ "${hostname#namenode}" = "${hostname}" ]; then
 fi
 
 #DataNode directories
-if ! [ "${hostname#datanode}" = "${hostname}" ]; then
+if ! [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
   echo "creating datanode directories";
   sudo mkdir -p $DFS_DATA_DIR;
   sudo chown -R $HDFS_USER:$HADOOP_GROUP $DFS_DATA_DIR;
@@ -93,31 +92,42 @@ echo "All HDFS hosts"
 sudo mkdir -p $HDFS_LOG_DIR
 sudo chown -R $HDFS_USER:$HADOOP_GROUP $HDFS_LOG_DIR
 sudo chmod -R 755 $HDFS_LOG_DIR
+
 sudo mkdir -p $HDFS_PID_DIR
 sudo chown -R $HDFS_USER:$HADOOP_GROUP $HDFS_PID_DIR
 sudo chmod -R 755 $HDFS_PID_DIR
 
 # All YARN hosts
 echo "All YARN hosts"
-sudo mkdir -p $YARN_LOG_DIR
-sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOG_DIR
-sudo chmod -R 755 $YARN_LOG_DIR
-sudo mkdir -p $YARN_PID_DIR
-sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_PID_DIR
-sudo chmod -R 755 $YARN_PID_DIR
-sudo mkdir -p $YARN_LOCAL_DIR
-sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOCAL_DIR
-sudo chmod -R 755 $YARN_LOCAL_DIR
+sudo mkdir -p $YARN_LOG_DIR;
+sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOG_DIR;
+sudo chmod -R 755 $YARN_LOG_DIR;
 
-# All YARN hosts MAPRED
-sudo mkdir -p $MAPRED_LOG_DIR
-sudo chown -R $YARN_USER:$HADOOP_GROUP $MAPRED_LOG_DIR
-sudo chmod -R 755 $MAPRED_LOG_DIR
+sudo mkdir -p $YARN_LOCAL_LOG_DIR;
+sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOCAL_LOG_DIR;
+sudo chmod -R 755 $YARN_LOCAL_LOG_DIR;
+
+sudo mkdir -p $YARN_PID_DIR;
+sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_PID_DIR;
+sudo chmod -R 755 $YARN_PID_DIR;
+
+sudo mkdir -p $YARN_LOCAL_DIR;
+sudo chown -R $YARN_USER:$HADOOP_GROUP $YARN_LOCAL_DIR;
+sudo chmod -R 755 $YARN_LOCAL_DIR;
+
+#JobHistory server logs
+sudo mkdir -p $MAPRED_LOG_DIR;
+sudo chown -R $YARN_USER:$HADOOP_GROUP $MAPRED_LOG_DIR;
+sudo chmod -R 755 $MAPRED_LOG_DIR;
+#JobHistory Server Process ID
+sudo mkdir -p $MAPRED_PID_DIR;
+sudo chown -R $MAPRED_USER:$HADOOP_GROUP $MAPRED_PID_DIR;
+sudo chmod -R 755 $MAPRED_PID_DIR;
 
 echo "Editing Hadoop core configuration files . . ."
 cd /tmp/hadoopOnGeni/configuration_files/core_hadoop
 sudo sed -i "s/TODO-NAMENODE-HOSTNAME/$HDFS_NAMENODE/g" ./core-site.xml
-sudo sed -i 's;TODO-FS-CHECKPOINT-DIR;'"$FS_CHECKPOINT_DIR"';g' ./core-site.xml
+#sudo sed -i 's;TODO-FS-CHECKPOINT-DIR;'"$FS_CHECKPOINT_DIR"';g' ./core-site.xml
 sudo sed -i 's;TODO-DFS-NAME-DIR;'"$DFS_NAME_DIR"';g' ./hdfs-site.xml
 sudo sed -i 's;TODO-DFS-DATA-DIR;'"$DFS_DATA_DIR"';g' ./hdfs-site.xml
 sudo sed -i "s/TODO-NAMENODE-HOSTNAME/$HDFS_NAMENODE/g" ./hdfs-site.xml
@@ -128,81 +138,81 @@ sudo sed -i "s/TODO-RMNODE-HOSTNAME/$YARN_RESOURCEMANAGER/g" ./yarn-site.xml
 sudo sed -i 's;TODO-YARN-LOCAL-DIR;'"$YARN_LOCAL_DIR"';g' ./yarn-site.xml
 sudo sed -i 's;TODO-YARN-LOG-DIR;'"$YARN_LOG_DIR"';g' ./yarn-site.xml
 
-# MR v1 on YARN
-sudo sed -i "s/TODO-RMNODE-HOSTNAME/$YARN_RESOURCEMANAGER/g" ./mapred-site.xml
+# MR on YARN
+sudo sed -i "s/TODO-JOBHISTORYNODE-HOSTNAME/$YARN_RESOURCEMANAGER/g" ./mapred-site.xml
+sudo sed -i "s/TODO-JOBHISTORYNODE-HOSTNAME/$YARN_RESOURCEMANAGER/g" ./mapred-site.xml
+#sudo sed -i 's/-XX:NewSize=[0-9*m|G]*/-XX:NewSize=64m/g' ./hadoop-env.sh
+#sudo sed -i 's/-XX:MaxNewSize=[0-9*m|G]*/-XX:MaxNewSize=64m/g' ./hadoop-env.sh
+#sudo sed -i 's/-Xmx[0-9*m|G]*/-Xmx512m/g' ./hadoop-env.sh
+#sudo sed -i 's/-Xms[0-9*m|G]*/-Xms512m/g' ./hadoop-env.sh
 
-sudo sed -i 's/-XX:NewSize=[0-9*m|G]*/-XX:NewSize=64m/g' ./hadoop-env.sh
-sudo sed -i 's/-XX:MaxNewSize=[0-9*m|G]*/-XX:MaxNewSize=64m/g' ./hadoop-env.sh
-sudo sed -i 's/-Xmx[0-9*m|G]*/-Xmx512m/g' ./hadoop-env.sh
-sudo sed -i 's/-Xms[0-9*m|G]*/-Xms512m/g' ./hadoop-env.sh
 
-# Workaround: NEED TO FIX THE BAD HEALTH CHECK: port 50060 instead of 8042
-sudo sed -i "s/50060/8042/g" ./health_check
+sudo touch $HADOOP_CONF_DIR/dfs.exclude
 
-sudo echo "$HDFS_DATANODE" | tr ',' '\n' > "$HADOOP_CONF_DIR/slaves"
 
 #All hosts
 echo "Deploying Hadoop core configuration files . . ."
 sudo rm -rf $HADOOP_CONF_DIR
 sudo mkdir -p $HADOOP_CONF_DIR
-sudo cp ./* $HADOOP_CONF_DIR
+sudo cp /tmp/hadoopOnGeni/configuration_files/core_hadoop/* $HADOOP_CONF_DIR
 sudo chmod a+x $HADOOP_CONF_DIR
 sudo chown -R $HDFS_USER:$HADOOP_GROUP $HADOOP_CONF_DIR/../
 sudo chmod -R 755 $HADOOP_CONF_DIR/../
 
-#workarounds
-#All hosts
-#sudo ln -s $HADOOP_LIB_DIR/libexec $YARN_LIB_DIR/
-#sudo ln -s $HADOOP_LIB_DIR/libexec $MAPRED_LIB_DIR/
-#TODO: HOW DO I SPECIFY THE LOG DIRECTION FOR MR ON YARN ???
-# THIS SHOULD NOT BE NECESSARY
-#sudo ln -s $MAPRED_LOG_DIR $MAPRED_LIB_DIR/logs
-
 #Boot up HDFS
 #NameNode
-if ! [ "${hostname#namenode}" = "${hostname}" ]; then
+if [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
   echo "namenode boot up HDFS";
   #formatting HDFS
-  su hdfs -c "echo Y| hdfs namenode -format;
+  sudo su $HDFS_USER -c "echo Y|/usr/hdp/current/hadoop-hdfs-namenode/../hadoop/bin/hdfs namenode -format";
   #starting HDFS
-  su hdfs -c "/usr/lib/hadoop/sbin/hadoop-daemon.sh start namenode;
+  sudo su $HDFS_USER -c "/usr/hdp/current/hadoop-hdfs-namenode/../hadoop/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR start namenode";
 
   #creating base folders in HDFS
-  su hdfs -c "/usr/lib/hadoop/bin/hadoop fs -mkdir -p /user/hdfs";
-  #su hdfs -c "/usr/lib/hadoop/bin/hadoop fs -mkdir -p /tmp";
-  #su hdfs -c "/usr/lib/hadoop/bin/hadoop fs -chmod +wt /tmp";
+  sudo su $HDFS_USER -c "hdfs dfs -mkdir -p /user/hdfs"
 fi
 
 #DataNode
-if ! [ "${hostname#datanode}" = "${hostname}" ]; then
+if ! [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
   echo "datanode starting HDFS";
   #starting HDFS
-  su hdfs -c "/usr/lib/hadoop/sbin/hadoop-daemon.sh start datanode;
+  sudo su $HDFS_USER -c "/usr/hdp/current/hadoop-hdfs-datanode/../hadoop/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR start datanode";
 fi
 
 #Boot up YARN
 # All hosts
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -mkdir -p /mapred/history/done_intermediate"
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -chmod -R 1777 /mapred/history/done_intermediate"
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -mkdir -p /mapred/history/done"
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -chmod -R 1777 /mapred/history/done"
-# notice yarn owns this folder. MR will not work because of permissions otherwise (to verify)
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -chown -R yarn /mapred"
-# This is a workaround for MR1 on YARN. Remove or alter config.
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -mkdir -p /app-logs"
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -chown -R yarn /app-logs"
-su $HDFS_USER -c "/usr/lib/hadoop/bin/hadoop fs -chmod -R +wt /app-logs"
+sudo su $HDFS_USER -c "hdfs dfs -mkdir -p /hdp/apps/$hdp_version/mapreduce/"
+sudo su $HDFS_USER -c "hdfs dfs -put /usr/hdp/current/hadoop-client/mapreduce.tar.gz /hdp/apps/$hdp_version/mapreduce/"
+sudo su $HDFS_USER -c "hdfs dfs -chown -R hdfs:hadoop /hdp"
+sudo su $HDFS_USER -c "hdfs dfs -chmod -R 555 /hdp/apps/$hdp_version/mapreduce"
+sudo su $HDFS_USER -c "hdfs dfs -chmod 444 /hdp/apps/$hdp_version/mapreduce/mapreduce.tar.gz"
+
 
 #YARN_RESOURCEMANAGER
-if ! [ "${hostname#namenode}" = "${hostname}" ]; then
-  su yarn -c "/usr/lib/hadoop-yarn/sbin/yarn-daemon.sh start resourcemanager"
-  su yarn -c "/usr/lib/hadoop-mapreduce/sbin/mr-jobhistory-daemon.sh start historyserver"
+if [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
+  sudo su -l yarn -c "/usr/hdp/current/hadoop-yarn-resourcemanager/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager"
+  #JobHistory server to set up directories on HDFS
+  sudo su $HDFS_USER -c "hdfs dfs -mkdir -p /mr-history/tmp";
+  sudo su $HDFS_USER -c "hdfs dfs -chmod -R 1777 /mr-history/tmp";
+  sudo su $HDFS_USER -c "hdfs dfs -mkdir -p /mr-history/done";
+  sudo su $HDFS_USER -c "hdfs dfs -chmod -R 1777 /mr-history/done";
+  sudo su $HDFS_USER -c "hdfs dfs -chown -R $MAPRED_USER:$HDFS_USER /mr-history";
+  sudo su $HDFS_USER -c "hdfs dfs -mkdir -p /app-logs";
+  sudo su $HDFS_USER -c "hdfs dfs -chmod -R 1777 /app-logs";
+  sudo su $HDFS_USER -c "hdfs dfs -chown $YARN_USER:$HDFS_USER /app-logs";
+  #JobHistory server
+  sudo su -l $YARN_USER -c "/usr/hdp/current/hadoop-mapreduce-historyserver/sbin/mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver";
 fi
 
 #YARN_NODEMANAGER
-if ! [ "${hostname#namenode}" = "${hostname}" ]; then
-  su yarn -c "/usr/lib/hadoop-yarn/sbin/yarn-daemon.sh start nodemanager"
+if ! [ "$(echo $(hostname) | cut -d. -f1)" = "namenode" ]; then
+  sudo su -l yarn -c "/usr/hdp/current/hadoop-yarn-nodemanager/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start nodemanager"
+  #change permissions on the container-executor file
+  sudo yarn -c "chown -R root:hadoop /usr/hdp/current/hadoop-yarn*/bin/container-executor";
+  sudo yarn -c "chmod -R 6050 /usr/hdp/current/hadoop-yarn*/bin/container-executor";
 fi
+
+
 
 #install ZooKeeper
 sudo yum -y install zookeeper
@@ -217,6 +227,10 @@ sudo mkdir -p $ZOOKEEPER_DATA_DIR
 sudo chown -R $ZOOKEEPER_USER:$HADOOP_GROUP $ZOOKEEPER_DATA_DIR
 sudo chmod -R 755 $ZOOKEEPER_DATA_DIR
 
+# initialize myid file
+# only work for single node zk cluster
+sudo echo '1' > $ZOOKEEPER_DATA_DIR/myid
+
 #ZooKeeper configuration
 echo "Editing ZooKeeper configuration files . . ."
 cd /tmp/hadoopOnGeni/configuration_files/zookeeper
@@ -227,7 +241,7 @@ sudo sed -i '/^dataDir/ c\dataDir='"$ZOOKEEPER_DATA_DIR"'' ./zoo.cfg
 #ZOOKEEPER2=`echo ZOOKEEPER_QUORUM | tr ',' '\n' | sed -n '2 p'`
 #ZOOKEEPER3=`echo ZOOKEEPER_QUORUM | tr ',' '\n' | sed -n '3 p'`
 
-sudo sed -i "s/TODO-ZOOKEEPER-SERVER-1/$ZOOKEEPER1/g" ./zoo.cfg
+sudo sed -i "s/TODO-ZOOKEEPER-SERVER-1/$ZOOKEEPER_QUORUM/g" ./zoo.cfg
 
 #if [ "$ZOOKEEPER2" == "" ]; then
 #    sudo sed -i '/TODO-ZOOKEEPER-SERVER-2/d' ./zoo.cfg;
@@ -248,4 +262,13 @@ sudo chmod a+x $ZOOKEEPER_CONF_DIR/
 sudo chown -R $ZOOKEEPER_USER:$HADOOP_GROUP $ZOOKEEPER_CONF_DIR/../
 sudo chmod -R 755 $ZOOKEEPER_CONF_DIR/../
 
-#install and configure startup scripts
+#startup scripts
+export HADOOP_LIBEXEC_DIR=/usr/lib/hadoop/libexec
+export JAVA_HOME=/usr/java/default
+export PATH=$JAVA_HOME/bin:$PATH
+
+#sudo zookeeper -c "/usr/lib/zookeeper/bin/zkServer.sh start /etc/zookeeper/conf/zoo.cfg"
+
+#sudo /usr/lib/hadoop-yarn/sbin/yarn-daemon.sh --config /etc/hadoop/conf start resourcemanager
+#sudo /usr/lib/hadoop-yarn/sbin/yarn-daemon.sh --config /etc/hadoop/conf start nodemanager
+#sudo /usr/lib/hadoop-mapreduce/sbin/mr-jobhistory-daemon.sh --config /etc/hadoop/conf start historyserver
